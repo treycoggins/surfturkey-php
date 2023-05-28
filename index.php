db1c776d4ed39f8d2069a7cc95fa1cce268e88b1
Trey Coggins <trey@surfturkey.net>
2023-05-28 01:36:03 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+echo 'Hsssellllo';
+
+// $requestPath = $_SERVER['REQUEST_URI'];
+
+// $basePath = '/Applications/MAMP/htdocs/surfturkey-php';
+// if (substr($requestPath, 0, strlen($basePath)) === $basePath) {
+//   $requestPath = substr($requestPath, strlen($basePath));
+// }
+
+// if ($requestPath === '/') {
+//   include 'pages/home.php';
+// } else if ($requestPath === '/about') {
+//   include 'pages/about.php';
+// } else if ($requestPath === '/blog') {
+//   include 'pages/blog.php';
+// } else if ($requestPath === '/contact') {
+//   include 'pages/contact.php';
+// } else if ($requestPath === '/projects') {
+//   include 'pages/projects.php';
+// } else if ($requestPath === '/get-started') {
+//   include 'pages/get-started.php';
+// } else {
+//   include 'pages/404.php';
+// }
+ ?>
---
+++ pages/projects.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include('includes/_head.php'); ?>
+
+<body id="projects" class="container">
+<?php include('includes/_navigation.php'); ?>
+
+  <header class="header">
+    <h1 class="heading-1 mt-xl">Projects</h1>
+    <h4 class="heading-4 mb-xl fade-in">Here at SurfTurkey.net we are constantly searching for new ways to incorporate
+      fun, new styles and functionality into our projects. Please click the thumbnail images to view our work!</h4>
+  </header>
+
+  <main class="main">
+    <div class="project">
+      <a class="project__link" href="projects/roots-reggae-music/index.html">
+        <img class="project__img" src="public/img/roots-reggae-music-port.png" alt="roots reggae music thumbnail">
+      </a>
+      <p class="project__text">We developed this website as a school project for a web design class. This project was
+        based on the Bootstrap CSS framework and includes many interactive components powered by Javascript.</p>
+    </div>
+
+    <div class="project">
+      <a class="project__link" href="projects/ct-lawncare/ct-lawncare.html">
+        <img class="project__img" src="public/img/ct-lawncare-port.png" alt="c & t lawn care thumbnail">
+      </a>
+      <p class="project__text">We developed this website for a fictional lawn care business. We are most proud of this
+        project as it incorporates features such as a contact form and a mobile-friendly layout.</p>
+    </div>
+
+    <div class="project">
+      <a class="project__link" href="projects/founders-gym/founders-gym.html">
+        <img class="project__img" src="public/img/founders-port.png" alt="founder's gym thumbnail image">
+      </a>
+      <p class="project__text">We developed this website for a fictional gym. This project features a mobile-friendly
+        layout as well as nice use of images.</p>
+    </div>
+
+    <div class="project">
+      <a class="project__link" href="projects/trillo/trillo.html">
+        <img class="project__img" src="public/img/trillo-port.png" alt="trillo travel agency thumbnail image">
+      </a>
+      <p class="project__text">We developed this website based on a design by Jonas Schmedtman. We learned a lot from
+        building this project. Notice the attention to detail and the attractive design elements.</p>
+    </div>
+    <div class="project">
+      <a class="project__link" href="projects/nexter/nexter.html">
+        <img class="project__img" src="public/img/nexter-port.png" alt="nexter homes thumbnail image">
+      </a>
+      <p class="project__text">Here is another website we developed from a design by Jonas Schmedtman. We really like
+        how
+        this project turned out for its use of color and spacious yet still mobile-friendly layout.</p>
+    </div>
+
+    <div class="project">
+      <a class="project__link" href="projects/natours/natours.html">
+        <img class="project__img" src="public/img/natours-port.png" alt="trillo travel agency thumbnail image">
+      </a>
+      <p class="project__text mb-sm">This project was our biggest under-taking during development. So many uniquely
+        designed components really makes this one stand out. Credit to Jonas Schmedtman for the design.</p>
+    </div>
+  </main>
+
+  
+  <?php include('includes/_footer.php'); ?>
+
+  <script src="src/js/app.js"></script>
+</body>
+
+</html>
